7d2c11d5c9b0137657e7fe92d2a425147c81b6c0
Liz Beacham & Tom Mertz <student@epicodus.com>
2015-03-02 14:36:46 -0800

    Add validation and cost to ship calculation

--- parcel.php
+++ parcel.php
@@ -55,6 +55,16 @@ class Parcel
         return $this->weight;
     }
 
+    function volume()
+    {
+        return $this->length * $this->width * $this->height;
+    }
+
+    function costToShip()
+    {
+        return $this->volume() * 0.1;
+    }
+
     function test() {
         echo $this->length;
         echo $this->width;
@@ -64,11 +74,39 @@ class Parcel
 
 }
 
-$package = new Parcel(14,44,32,21);
-
-$package->test();
-
-$package->setWeight(123);
-
-echo "<br />" .$package->getWeight();
 ?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <title>Your package info</title>
+    </head>
+    <body>
+        <h1>Here are your details!</h1>
+        <ul>
+            <?php
+                if($_GET["length"] != 0 && $_GET["width"] != 0 && $_GET["height"] != 0 && $_GET["weight"])
+                {
+                    //create the parcel object if it has all the inputs
+                    $package = new Parcel($_GET["length"], $_GET["width"], $_GET["height"], $_GET["weight"]);
+
+                    echo "<li> Package volume: " . $package->volume() . "</li>";
+                    echo "<ul>";
+                        echo "<li>Length: " . $package->getLength() . "</li>";
+                        echo "<li>Width: " . $package->getWidth() . "</li>";
+                        echo "<li>Height: " . $package->getHeight() . "</li>";
+                    echo "</ul>";
+                    echo "<li> Shipping cost: " . $package->costToShip() . "</li>";
+                }
+                else {
+                    echo "<p>ERROR: You need to complete all inputs.</p>";
+                }
+
+
+            ?>
+        </ul>
+
+    </body>
+</html>
