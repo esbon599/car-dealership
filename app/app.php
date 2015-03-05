d137ebe5440951d85c402da0595d51b5042d4c09
Tom Mertz & Nhu Finney <student@epicodus.com>
2015-03-05 10:43:59 -0800

    Add car postings page

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/car.php";
 
     $app = new Silex\Application();
 
@@ -10,6 +11,14 @@
     $app->get("/", function() use ($app) {
         return $app['twig']->render('car_form.php');
 
+    });
+
+    $app->post("/result", function() use ($app) {
+        $car = new Car($_POST['model'], $_POST['price'], $_POST['miles'], $_POST['image']);
+        var_dump($car);
+
+
+        return $app['twig']->render('result.php');
     });
 
     return $app;
